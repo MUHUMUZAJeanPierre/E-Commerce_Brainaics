5ac6d85dd4c069a2d8d55ec63d68c908d7f89cc8
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2024-07-05 15:43:16 +0200

    feat/cart

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -3,8 +3,84 @@ import {addToCart, getCart, removeFromCart} from '../controllers/cartController.
 import authMiddleware from '../middleware/auth.js';
 const cartRouter = express.Router();
 
-cartRouter.post("/add", authMiddleware ,addToCart);
-cartRouter.post("/remove", authMiddleware,removeFromCart);
-cartRouter.post("/get", authMiddleware, getCart);
+/**
+ * @swagger
+ * /cart/add:
+ *   post:
+ *     summary: Add item to cart
+ *     tags:
+ *       - Cart
+ *     security:
+ *       - BearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               itemId:
+ *                 type: string
+ *               quantity:
+ *                 type: integer
+ *             required:
+ *               - itemId
+ *               - quantity
+ *     responses:
+ *       200:
+ *         description: Item added to cart successfully
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: Unauthorized
+ */
+cartRouter.post('/add', authMiddleware, addToCart);
+
+/**
+ * @swagger
+ * /cart/remove:
+ *   post:
+ *     summary: Remove item from cart
+ *     tags:
+ *       - Cart
+ *     security:
+ *       - BearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               itemId:
+ *                 type: string
+ *             required:
+ *               - itemId
+ *     responses:
+ *       200:
+ *         description: Item removed from cart successfully
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: Unauthorized
+ */
+cartRouter.post('/remove', authMiddleware, removeFromCart);
+
+/**
+ * @swagger
+ * /cart/get:
+ *   post:
+ *     summary: Get user's cart
+ *     tags:
+ *       - Cart
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Return user's cart
+ *       401:
+ *         description: Unauthorized
+ */
+cartRouter.post('/get', authMiddleware, getCart);
 
 export default cartRouter;
